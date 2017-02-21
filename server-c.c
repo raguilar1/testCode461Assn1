02f891f8a56d55f05b0efef9bc9efd80ee84e272
raguilar1 <raguilar@princeton.edu>
2017-02-20 22:12:25 -0500

    salad

--- server-c.c
+++ server-c.c
@@ -46,7 +46,7 @@ int server(char *server_port) {
 		new_fd = accept(s, (struct sockaddr *)&their_addr, &addr_size);
 		success = 1;
 		while(1){
-			success = recv(new_fd, *buf, RECV_BUFFER_SIZE, 0);
+			success = recv(new_fd, buf, RECV_BUFFER_SIZE, 0);
 			if(success == -1 || success == 0){
 				break;
 			}
